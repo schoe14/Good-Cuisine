81b4524db6cc8270f44e59b042abd4d15c1af647
pb-totoro <jonesec2@gmail.com>
2020-02-19 22:25:16 -0500

    Set up handlebars, server routes, and recipe search js

---
+++ routes/saved-searches.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+// const Recipe = require('../models')
+
+router.get('/', (req, res) => res.render('saved'));
+
+
+
+module.exports = router
