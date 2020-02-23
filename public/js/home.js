e25b3d7fb5799a5a3a79021a8bf3189e69d8ae76
Seohui Choe <chelsea.choe@gmail.com>
2020-02-23 18:00:16 -0500

    Creat validation message from server for log-in/sign-up

--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,31 @@
 console.log("home.js loaded");
 
+// To check if input is null
+function isEmpty(value) {
+    return value === "";
+}
+
+$("#go-home").on("click", function (event) {
+    event.preventDefault();
+    console.log("hello")
+    $("#go-home").animateCss('pulse', function () {
+        window.location.href = "/"
+    });
+});
+
 $("#search-recipe").on("click", function (event) {
     event.preventDefault();
     $("#search-recipe").animateCss('pulse', function () {
-        //callback (things to do after all animations are done)
         window.location.href = "/search"
     });
 });
 
-$("#view-saved-recipes").on("click", function (event) {
-    event.preventDefault();
-    $("#view-saved-recipes").animateCss('pulse', function () {
-        window.location.href = "/saved-recipes"
-    });
-});
+// $("#view-saved-recipes").on("click", function (event) {
+//     event.preventDefault();
+//     $("#view-saved-recipes").animateCss('pulse', function () {
+//         window.location.href = "/saved-recipes"
+//     });
+// });
 
 $("#sign-up").on("click", function (event) {
     event.preventDefault();
@@ -24,15 +36,10 @@ $("#sign-up").on("click", function (event) {
 
 $("#sign-in-modal").on("click", function (event) {
     event.preventDefault();
+    $("#email").val("");
+    $("#password").val("");
+    $("#err-msg").empty("");
     $("#account-info").modal("show");
-});
-
-$("#go-home").on("click", function (event) {
-    event.preventDefault();
-    console.log("hello")
-    $("#go-home").animateCss('pulse', function () {
-        window.location.href = "/"
-    });
 });
 
 $("#sign-in").on("click", function (event) {
@@ -41,15 +48,32 @@ $("#sign-in").on("click", function (event) {
         email: $("#email").val().trim(),
         password: $("#password").val().trim()
     }
-    $.post("/login", user, function (results) {
-        if (results) {
-            // $(location).attr('href', '/accounts/view')
-            $(location).attr('href', '/')
-        } else {
-            $("#account-info").modal("close");
-            alert("oops something went wrong, please try again!");
-        }
-    })
+    console.log("email", user.email, "password", user.password);
+    if (!isEmpty(user.email) && !isEmpty(user.password)) {
+        $.ajax({
+            type: "POST",
+            url: "/login",
+            data: user
+        }).then(function (data) {
+            console.log(data);
+            console.log("data.message ", data.message);
+            $("#err-msg").empty("").text(data.message);
+            if (data === true) window.location.href = "/search";
+        });
+    }
+    else {
+        console.log("fill out entire form");
+        $("#err-msg").empty("").text("Email and password cannot be empty");
+    }
+    // $.post("/login", user, function (results) {
+    //     if (results) {
+    //         // $(location).attr('href', '/accounts/view')
+    //         $(location).attr('href', '/')
+    //     } else {
+    //         $("#account-info").modal("close");
+    //         alert("oops something went wrong, please try again!");
+    //     }
+    // })
 });
 
 $("#view-account").on("click", function (event) {
